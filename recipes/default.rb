820da8fe3ffc8e2e85aa31ed9fdeb9382535817f
Brian Bergstrom <brian.bergstrom@sportngin.com>
2014-12-10 12:14:06 -0600

    Refactoring conditional install to be more chefy.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,19 +1,19 @@
-`ldconfig -v 2>&1 | grep -q libfreeimage`
-installed = $?.to_i == 0
 
-unless installed
-  remote_file '/tmp/freeimage.zip' do
-    source node[:freeimage][:source][:url]
-  end
+remote_file 'freeimage.zip' do
+  path '/tmp/freeimage.zip'
+  source node[:freeimage][:source][:url]
+  not_if 'ldconfig -v 2>&1 | grep -q libfreeimage'
+end
 
-  execute 'Compile, install and clean freeimage' do
-    cwd '/tmp'
-    command <<-EOF
-  unzip freeimage.zip
-  cd FreeImage
-  make
-  make install
-  make clean
-    EOF
-  end
+execute 'Compile, install and clean freeimage' do
+  cwd '/tmp'
+  command <<-EOF
+unzip freeimage.zip
+cd FreeImage
+make
+make install
+make clean
+  EOF
+  action :nothing
+  subscribes :run, 'remote_file[freeimage.zip]', :immediately
 end
